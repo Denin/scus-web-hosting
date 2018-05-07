ed0ece7856046b6132d2c1ae0c33d981f9b3dc8e
Tim Stanley <tstanleync@gmail.com>
2018-05-06 21:33:31 -0700

    Initial load for testing

---
+++ business-hours/src/app.js
@@ -0,0 +1,22 @@
+﻿var siteUrl = config.apiUri + '/site/' + config.siteId;
+
+alert(siteUrl);
+
+//$.get(siteUrl,'', {'Authorization' : oauth.sign("GET", siteUrl)});
+
+var endpoint = URI.parse(siteUrl);
+console.log('URL: ' + 'https://' + siteUrl);
+
+$.ajax(
+    {
+        url: siteUrl,
+        type: 'get',
+        headers: {
+            'Authorization': oauth.sign("GET", 'https://' + siteUrl)
+        },
+        dataType: 'json',
+        success: function (data) {
+            console.log(data);
+        }
+    }
+)
---
+++ business-hours/src/config.js
@@ -0,0 +1,11 @@
+﻿var config = {
+    "widgetBaseUri": "d19ujuohqco9tx.cloudfront.net/webwidgets/7-14-0-39800",
+    "apiUri": "https://pos.shortcutssoftware.com/webapi",
+    "apiVersion": "7.14.0",
+    "culture": "en-us",
+    "consumerKey": "jv6YflwoCatKYxBNffV5",
+    "consumerSecret": "YzIQtHDfqHoecCbua6GM",
+    "accessTokenKey": "nJjQdJiPxDYfxfmqfM4S",
+    "accessTokenSecret": "K6ZXhm8EYcdFec85fcj2",
+    "siteId": "40487"
+}
